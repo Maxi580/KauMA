b546e3421b03b9de413303b618a4733cc74169e3
Maxi580 <maxieichenseher@gmail.com>
2024-11-23 21:49:57 +0100

    Finished working gcm_crack

--- gcm/gcm_crack.py
+++ gcm/gcm_crack.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
+from imp import get_tag
 
 from block_poly.b64_block import B64
 from block_poly.block import Block
 from galoisfield.galoisfieldelement import GaloisFieldElement
 from gcm.bruteforce_test import ciphertext
-from crypto_algorithms.gcm import get_l, get_ghash, get_auth_key
+from crypto_algorithms.gcm import get_l, get_ghash, get_auth_key, calculate_tag
 from galoisfield.galoisfieldpolynomial import GaloisFieldPolynomial
 from gcm.find_roots import find_roots
 
@@ -14,6 +15,14 @@ class GCMMessage:
     ciphertext: GaloisFieldPolynomial
     associated_data: GaloisFieldPolynomial
     tag: GaloisFieldElement
+    original_cipher_length: int
+    original_ad_length: int
+
+
+@dataclass
+class GCMForgery:
+    ciphertext: GaloisFieldPolynomial
+    associated_data: GaloisFieldPolynomial
     original_cipher_length: int
     original_ad_length: int
 
@@ -30,7 +39,18 @@ def json_to_gcm_message(message_data: dict) -> GCMMessage:
     )
 
 
-def get_zeroed_poly(message: GCMMessage) -> GaloisFieldPolynomial:
+def json_to_gcm_forgery_message(message_data: dict) -> GCMForgery:
+    cipher_bytes = B64(message_data["ciphertext"]).block
+    ad_bytes = B64(message_data["associated_data"]).block
+    return GCMForgery(
+        ciphertext=GaloisFieldPolynomial.from_block(cipher_bytes),
+        associated_data=GaloisFieldPolynomial.from_block(ad_bytes),
+        original_cipher_length=len(cipher_bytes),
+        original_ad_length=len(ad_bytes)
+    )
+
+
+def _get_zeroed_poly(message: GCMMessage) -> GaloisFieldPolynomial:
     """returns 0 = cnHn + cn−1Hn−1 + . . . + c2H2 + LH + T"""
     poly = GaloisFieldPolynomial([])
 
@@ -48,10 +68,27 @@ def get_zeroed_poly(message: GCMMessage) -> GaloisFieldPolynomial:
     return poly
 
 
-def gcm_crack(nonce: bytes, m1: GCMMessage, m2: GCMMessage, m3: GCMMessage, forgery_ciphertext: bytes,
-              forgery_ad: bytes):
-    Tu = get_zeroed_poly(m1)
-    Tv = get_zeroed_poly(m2)
+def _find_correct_h(h_candidates: list[GaloisFieldElement], m1: GCMMessage, m3: GCMMessage) \
+        -> tuple[GaloisFieldElement, GaloisFieldElement]:
+    for potential_auth_key in h_candidates:
+        # Calculate back the ek0 for the given auth key, stays the same due to same nonce etc.
+        m1_l = get_l(m1.original_ad_length, m1.original_cipher_length)
+        m1_ghash = get_ghash(potential_auth_key, m1.associated_data, m1.ciphertext, m1_l)
+        ek0 = m1_ghash + m1.tag
+
+        # Try to authenticate m3 with potential auth key
+        m3_l = get_l(m3.original_ad_length, m3.original_cipher_length)
+        m3_ghash = get_ghash(potential_auth_key, m3.associated_data, m3.ciphertext, m3_l)
+        tag = ek0 + m3_ghash
+
+        # If Tag is the same, authentication is successful
+        if tag == m3.tag:
+            return potential_auth_key, ek0
+
+
+def gcm_crack(nonce: bytes, m1: GCMMessage, m2: GCMMessage, m3: GCMMessage, forgery: GCMForgery):
+    Tu = _get_zeroed_poly(m1)
+    Tv = _get_zeroed_poly(m2)
 
     F = Tu - Tv
     F.make_monic()
@@ -59,22 +96,15 @@ def gcm_crack(nonce: bytes, m1: GCMMessage, m2: GCMMessage, m3: GCMMessage, forg
     roots = find_roots(F)
     h_candidates = [root[0] for root in roots]
 
-    correct_auth_key = None
-    for potential_auth_key in h_candidates:
-        # Calculate back the ek0 for the given auth key, stays the same due to same nonce etc.
-        m1_l = get_l(m1.original_ad_length, m1.original_cipher_length)
-        m1_ghash = get_ghash(potential_auth_key, m1.associated_data, m1.ciphertext, m1_l)
-        ek0 = m1_ghash + m1.tag
+    correct_h, mask = _find_correct_h(h_candidates, m1, m3)
+    assert correct_h is not None, "No Correct auth Key has been found"
 
-        # Try to authenticate m3 with potential auth key (check if real tag is the same
-        m3_l = get_l(m3.original_ad_length, m3.original_cipher_length)
-        m3_ghash = get_ghash(potential_auth_key, m3.associated_data, m3.ciphertext, m3_l)
-        tag = ek0 + m3_ghash
+    forgery_l = get_l(forgery.original_ad_length, forgery.original_cipher_length)
+    forgery_ghash = get_ghash(correct_h, forgery.associated_data, forgery.ciphertext, forgery_l)
+    forgery_tag = forgery_ghash + mask
 
-        if tag == m3.tag:
-            correct_auth_key = potential_auth_key
-            break
+    print(correct_h.to_b64_gcm())
+    print(mask.to_b64_gcm())
+    print(forgery_tag.to_b64_gcm())
 
-    assert correct_auth_key is not None, "No Correct auth Key has been found"
-
-    print(correct_auth_key.to_b64_gcm())
+    return forgery_tag, correct_h, mask
--- kauma.py
+++ kauma.py
@@ -14,7 +14,7 @@ from paddingoracle.paddingOracle import padding_oracle_attack
 from galoisfield.galoisfieldelement import GaloisFieldElement
 from galoisfield.galoisfieldpolynomial import GaloisFieldPolynomial
 from gcm.find_roots import sff, ddf, edf
-from gcm.gcm_crack import gcm_crack, json_to_gcm_message
+from gcm.gcm_crack import gcm_crack, json_to_gcm_message, json_to_gcm_forgery_message
 
 ENCRYPT_MODE = "encrypt"
 DECRYPT_MODE = "decrypt"
@@ -252,13 +252,11 @@ def gcm_crack_action(arguments: Dict[str, Any]) -> Dict[str, Any]:
     m2 = json_to_gcm_message(arguments["m2"])
     m3 = json_to_gcm_message(arguments["m3"])
 
-    forgery_data = arguments["forgery"]
-    forgery_ciphertext = B64(forgery_data["ciphertext"]).block
-    forgery_ad = B64(forgery_data["associated_data"]).block
+    forgery = json_to_gcm_forgery_message(arguments["forgery"])
 
-    tag, H, mask = gcm_crack(nonce, m1, m2, m3, forgery_ciphertext, forgery_ad)
+    tag, H, mask = gcm_crack(nonce, m1, m2, m3, forgery)
 
-    return {"tag": tag, "H": H, "mask": mask}
+    return {"tag": tag.to_b64_gcm(), "H": H.to_b64_gcm(), "mask": mask.to_b64_gcm()}
 
 
 ACTION_PROCESSORS = {
